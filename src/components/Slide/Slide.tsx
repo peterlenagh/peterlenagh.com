9ea51e8aa1993a8fa4da6c93e16f789fe31fe208
Peter Lenagh <peter@usemarkup.com>
2023-03-07 11:31:33 +0000

    fix(presentations): allow overflow scroll on slides.

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,27 +1,22 @@
-"use client";
+// "use client";
 
 import React from 'react';
 
 import classnames from 'classnames/bind';
 
-import Button from '@/components/Button';
-
 import styles from './Slide.module.scss';
 
 const cx = classnames.bind(styles);
 
-type SlideProps = React.ComponentPropsWithoutRef<'section'> & {
-  mini?: boolean;
-};
+type SlideProps = React.ComponentPropsWithoutRef<'section'>;
 
-const Slide = ({ className, children, mini = false, ...props }: SlideProps) => {
+const Slide = ({ className, children, ...props }: SlideProps) => {
   return (
-    <section className={cx('slide', 'markdown-body', { 'slide--mini': mini }, className)} {...props}>
+    <section className={cx('slide', 'markdown-body', className)} {...props}>
       <div className={cx('slide__contents')}>
         {children}
         </div>
     </section>
-    // <Button label="sigh"/>
   );
 };
 
